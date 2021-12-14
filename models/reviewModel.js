0b8e83af725003dd6e7554e5d5fe2e193870ebf1
drammehjunior <mameddram@gmail.com>
2021-12-15 02:41:17 +0700

    added UNIQUE index for reviews in the model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -45,6 +45,8 @@ const reviewSchema = mongoose.Schema({
 //     next();
 // });
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 reviewSchema.pre(/^find/, function(next){
     this.populate({
         path: 'user',
